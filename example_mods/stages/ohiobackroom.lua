ae7665436feff33ec2cdb908a5610aa89fce362d
Smokey <78874128+Smokey555@users.noreply.github.com>
2024-06-03 02:51:35 +0200

    Backrooms

---
+++ example_mods/stages/ohiobackroom.lua
@@ -0,0 +1,15 @@
+function onCreate()
+		makeLuaSprite('grad', 'ohio/grad', 200, 560);
+	addLuaSprite('grad', true);
+setProperty('ohio.antialiasing', false);
+
+
+		makeLuaSprite('ohio', 'ohio/backroom1', 200, 560);
+	addLuaSprite('ohio', false);
+setProperty('ohio.antialiasing', false);
+
+makeLuaSprite('gf', 'ohio/gf', 450, 720);
+	addLuaSprite('gf', false);
+setProperty('gf.antialiasing', false);
+
+end
