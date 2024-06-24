b5d80d9f8fc0aaffb2a004146fbc38e612af00d8
croc <82195258+crocidy@users.noreply.github.com>
2024-06-24 06:43:53 -0400

    poopsies

--- example_mods/stages/rio.lua
+++ example_mods/stages/rio.lua
@@ -1,4 +1,10 @@
 function onCreate()
+    setProperty("skipCountdown", true)
     makeLuaSprite("bg",'bg/rio/rio', -745,-250)
     addLuaSprite("bg")
+    makeLuaSprite("overlay",'bg/rio/rio', -745,-250)
+    addLuaSprite("overlay")
+    setObjectCamera('overlay', 'camother')
+    setBlendMode("overlay", "add")
+    setProperty('overlay.alpha', 0.6)
 end
---
+++ example_mods/stages/stage_clone.lua
@@ -0,0 +1,5 @@
+function onCreate()
+	makeLuaSprite('skibidistage', 'bg/skibidi/skibidibgstage', -175, 120);
+	setScrollFactor('skibidibgstage', 0.3, 0.3);
+	addLuaSprite('skibidistage', false);
+end
