c0683490db4fdf1ce2028caeacd4691887722dc6
croc <82195258+crocidy@users.noreply.github.com>
2024-06-24 00:03:50 -0400

    fix

---
+++ example_mods/stages/hollup.lua
@@ -0,0 +1,52 @@
+function onCreate()
+
+
+    makeLuaSprite("bg",'bg/bop/blue', -745,-250)
+    addLuaSprite("bg")
+    setScrollFactor('bg', 1.5, 1);
+    scaleObject('bg', 1.1, 1);
+    makeLuaSprite("plane",'bg/bop/plane', 10000 , 200)
+    addLuaSprite("plane")
+    makeLuaSprite("birds",'bg/bop/birds',1000, 30)
+    addLuaSprite("birds")
+    setScrollFactor('birds', 1.2, 1);
+    makeLuaSprite("sun",'bg/bop/sun', -300, 0)
+    addLuaSprite("sun")
+
+    makeLuaSprite("city",'bg/bop/city',  -980,150)
+    addLuaSprite("city")
+    setScrollFactor('city', 1.3, 1);
+    scaleObject('city', 1.5, 1);
+
+    makeLuaSprite("grass",'bg/bop/grass',  -845,550)
+    addLuaSprite("grass")
+    scaleObject('grass', 1.2, 1);
+
+    makeLuaSprite("cloud 1",'bg/bop/cloud 1',1000, -230)
+    addLuaSprite("cloud 1")
+    setScrollFactor('cloud 1', 1.21, 1);
+
+    makeLuaSprite("cloud 2",'bg/bop/cloud 2', -450, 30)
+    addLuaSprite("cloud 2")
+    setScrollFactor('cloud 2', 1.2, 1);
+end
+
+function onCreatePost()
+    setObjectOrder('gfGroup', getObjectOrder('dadGroup')+1)
+end
+function onSectionHit()
+
+    if getRandomBool(500) then
+    plane()
+    end
+end
+
+function plane()
+    doTweenX('plane','plane',-2150,5.4,'sineOut')
+end
+
+function onBeatHit()
+    if curBeat == 224 then  
+        doTweenX('ben', 'gf', 2220, 2, 'sineOut')
+    end 
+end 
